fdf3973e7cbfcdc3aeef815e0fa9864551a495a5
psycho-manav <manavhere13@gmail.com>
2024-11-08 13:52:12 +0530

    Fix bad interpreter errors

--- linexpchecker.py
+++ linexpchecker.py
@@ -58,6 +58,7 @@ enlightenment_url = "http://www.grsecurity.net/~spender/enlightenment.tgz"
 version = "1.4"
 scriptname = os.path.basename(sys.argv[0])
 
+
 def fix_version(version):
     split_version = version.split(".")
 
@@ -68,64 +69,66 @@ def fix_version(version):
 
     return version
 
+
 def main():
-    print "%s v%s\n" % (scriptname, version)
+    print("%s v%s\n") % (scriptname, version)
 
     if len(sys.argv) == 2:
         if sys.argv[1] == '-h' or sys.argv[1] == '--help':
-            print "Usage: %s [kernel-version]" % scriptname
+            print("Usage: %s [kernel-version]") % scriptname
             sys.exit(0)
         else:
             kernel_version_string = sys.argv[1]
-            print "[*] Given kernel version: %s" % kernel_version_string
+            print("[*] Given kernel version: %s") % kernel_version_string
     else:
         kernel_version_string = os.popen('uname -r').read().strip()
-        print "[*] Automatically detected local kernel version: %s" % kernel_version_string
+        print(
+            "[*] Automatically detected local kernel version: %s") % kernel_version_string
 
     kernel_parts = kernel_version_string.split("-")
     kernel_version = fix_version(kernel_parts[0])
     found_exploit = False
     exploits = {
-                 "do_brk": { "CVE": "2003-0961", "versions": ("2.4.0-2.4.22",), "exploits": (131,) },
-                 "mremap missing do_munmap": { "CVE": "2004-0077", "versions": ("2.2.0-2.2.25", "2.4.0-2.4.24", "2.6.0-2.6.2"), "exploits": (160,) },
-                 "binfmt_elf Executable File Read": { "CVE": "2004-1073", "versions": ("2.4.0-2.4.27", "2.6.0-2.6.8"), "exploits": (624,) },
-                 "uselib()": { "CVE": "2004-1235", "versions": ("2.4.0-2.4.29rc2", "2.6.0-2.6.10rc2"), "exploits": (895,) },
-                 "bluez": { "CVE": "2005-1294", "versions": ("2.6.0-2.6.11.5",), "exploits": (4756, 926) },
-                 "prctl()": { "CVE": "2006-2451", "versions": ("2.6.13-2.6.17.4",), "exploits": (2031, 2006, 2011, 2005, 2004) }, 
-                 "proc": { "CVE": "2006-3626", "versions": ("2.6.0-2.6.17.4",), "exploits": (2013,) },
-                 "system call emulation": { "CVE": "2007-4573", "versions": ("2.4.0-2.4.30", "2.6.0-2.6.22.7",), "exploits": (4460,) },
-                 "vmsplice": { "CVE": "2008-0009", "versions": ("2.6.17-2.6.24.1",), "exploits": (5092, 5093) },
-                 "ftruncate()/open()": { "CVE": "2008-4210", "versions": ("2.6.0-2.6.22",), "exploits": (6851,) },
-                 "eCryptfs (Paokara)": { "CVE": "2009-0269", "versions": ("2.6.19-2.6.31.1",), "exploits": (enlightenment_url,) },
-                 "set_selection() UTF-8 Off By One": { "CVE": "2009-1046", "versions": ("2.6.0-2.6.28.3",), "exploits": (9083,) },
-                 "UDEV < 141": { "CVE": "2009-1185", "versions": ("2.6.25-2.6.30",), "exploits": (8478, 8572) },
-                 "exit_notify()": { "CVE": "2009-1337", "versions": ("2.6.0-2.6.29",), "exploits": (8369,) },
-                 "ptrace_attach() Local Root Race Condition": { "CVE": "2009-1527", "versions": ("2.6.29",), "exploits": (8678, 8673) },
-                 "sock_sendpage() (Wunderbar Emporium)": { "CVE": "2009-2692", "versions": ("2.6.0-2.6.31rc3", "2.4.0-2.4.37.1"), "exploits": (9641, 9545, 9479, 9436, 9435, enlightenment_url) },
-                 "udp_sendmsg() (The Rebel)": { "CVE": "2009-2698", "versions": ("2.6.0-2.6.9.2",), "exploits": (9575, 9574, enlightenment_url) },
-                 "(32bit) ip_append_data() ring0": { "CVE": "2009-2698", "versions": ("2.6.0-2.6.9",), "exploits": (9542,) },
-                 "perf_counter_open() (Powerglove and Ingo m0wnar)": { "CVE": "2009-3234", "versions": ("2.6.31",), "exploits": (enlightenment_url,) },
-                 "pipe.c (MooseCox)": { "CVE": "2009-3547", "versions": ("2.6.0-2.6.32rc5", "2.4.0-2.4.37"), "exploits": (10018, enlightenment_url) },
-                 "CPL 0": { "CVE": "2010-0298", "versions": ("2.6.0-2.6.11",), "exploits": (1397,) },
-                 "ReiserFS xattr": { "CVE": "2010-1146", "versions": ("2.6.0-2.6.34rc3",), "exploits": (12130,) },
-                 "Unknown": { "CVE": None, "versions": ("2.6.18-2.6.20",), "exploits": (10613,) },
-                 "SELinux/RHEL5 (Cheddar Bay)": { "CVE": None, "versions": ("2.6.9-2.6.30",), "exploits": (9208, 9191, enlightenment_url) },
-                 "compat": { "CVE": "2010-3301", "versions": ("2.6.27-2.6.36rc4",), "exploits": (15023, 15024) },
-                 "BCM": { "CVE": "2010-2959", "versions": ("2.6.0-2.6.36rc1",), "exploits": (14814,) },
-                 "RDS protocol": { "CVE": "2010-3904", "versions": ("2.6.0-2.6.36rc8",), "exploits": (15285,) },
-                 "put_user() - full-nelson": { "CVE": "2010-4258", "versions": ("2.6.0-2.6.37",), "exploits": (15704,) },
-                 "sock_no_sendpage() - full-nelson": { "CVE": "2010-3849", "versions": ("2.6.0-2.6.37",), "exploits": (15704,) },
-                 "ACPI custom_method": { "CVE": "2010-4347", "versions": ("2.6.0-2.6.37rc2",), "exploits": (15774,) },
-                 "CAP_SYS_ADMIN": { "CVE": "2010-4347", "versions": ("2.6.34-2.6.37",), "exploits": (15916, 15944) },
-                 "econet_sendmsg() - half-nelson": { "CVE": "2010-3848", "versions": ("2.6.0-2.6.36.2",), "exploits": (17787,) },
-                 "ec_dev_ioctl() - half-nelson": { "CVE": "2010-3850", "versions": ("2.6.0-2.6.36.2",), "exploits": (17787, 15704) },
-                 "ipc - half-nelson": { "CVE": "2010-4073", "versions": ("2.6.0-2.6.37rc1",), "exploits": (17787,) },
-               }
+        "do_brk": {"CVE": "2003-0961", "versions": ("2.4.0-2.4.22",), "exploits": (131,)},
+        "mremap missing do_munmap": {"CVE": "2004-0077", "versions": ("2.2.0-2.2.25", "2.4.0-2.4.24", "2.6.0-2.6.2"), "exploits": (160,)},
+        "binfmt_elf Executable File Read": {"CVE": "2004-1073", "versions": ("2.4.0-2.4.27", "2.6.0-2.6.8"), "exploits": (624,)},
+        "uselib()": {"CVE": "2004-1235", "versions": ("2.4.0-2.4.29rc2", "2.6.0-2.6.10rc2"), "exploits": (895,)},
+        "bluez": {"CVE": "2005-1294", "versions": ("2.6.0-2.6.11.5",), "exploits": (4756, 926)},
+        "prctl()": {"CVE": "2006-2451", "versions": ("2.6.13-2.6.17.4",), "exploits": (2031, 2006, 2011, 2005, 2004)},
+        "proc": {"CVE": "2006-3626", "versions": ("2.6.0-2.6.17.4",), "exploits": (2013,)},
+        "system call emulation": {"CVE": "2007-4573", "versions": ("2.4.0-2.4.30", "2.6.0-2.6.22.7",), "exploits": (4460,)},
+        "vmsplice": {"CVE": "2008-0009", "versions": ("2.6.17-2.6.24.1",), "exploits": (5092, 5093)},
+        "ftruncate()/open()": {"CVE": "2008-4210", "versions": ("2.6.0-2.6.22",), "exploits": (6851,)},
+        "eCryptfs (Paokara)": {"CVE": "2009-0269", "versions": ("2.6.19-2.6.31.1",), "exploits": (enlightenment_url,)},
+        "set_selection() UTF-8 Off By One": {"CVE": "2009-1046", "versions": ("2.6.0-2.6.28.3",), "exploits": (9083,)},
+        "UDEV < 141": {"CVE": "2009-1185", "versions": ("2.6.25-2.6.30",), "exploits": (8478, 8572)},
+        "exit_notify()": {"CVE": "2009-1337", "versions": ("2.6.0-2.6.29",), "exploits": (8369,)},
+        "ptrace_attach() Local Root Race Condition": {"CVE": "2009-1527", "versions": ("2.6.29",), "exploits": (8678, 8673)},
+        "sock_sendpage() (Wunderbar Emporium)": {"CVE": "2009-2692", "versions": ("2.6.0-2.6.31rc3", "2.4.0-2.4.37.1"), "exploits": (9641, 9545, 9479, 9436, 9435, enlightenment_url)},
+        "udp_sendmsg() (The Rebel)": {"CVE": "2009-2698", "versions": ("2.6.0-2.6.9.2",), "exploits": (9575, 9574, enlightenment_url)},
+        "(32bit) ip_append_data() ring0": {"CVE": "2009-2698", "versions": ("2.6.0-2.6.9",), "exploits": (9542,)},
+        "perf_counter_open() (Powerglove and Ingo m0wnar)": {"CVE": "2009-3234", "versions": ("2.6.31",), "exploits": (enlightenment_url,)},
+        "pipe.c (MooseCox)": {"CVE": "2009-3547", "versions": ("2.6.0-2.6.32rc5", "2.4.0-2.4.37"), "exploits": (10018, enlightenment_url)},
+        "CPL 0": {"CVE": "2010-0298", "versions": ("2.6.0-2.6.11",), "exploits": (1397,)},
+        "ReiserFS xattr": {"CVE": "2010-1146", "versions": ("2.6.0-2.6.34rc3",), "exploits": (12130,)},
+        "Unknown": {"CVE": None, "versions": ("2.6.18-2.6.20",), "exploits": (10613,)},
+        "SELinux/RHEL5 (Cheddar Bay)": {"CVE": None, "versions": ("2.6.9-2.6.30",), "exploits": (9208, 9191, enlightenment_url)},
+        "compat": {"CVE": "2010-3301", "versions": ("2.6.27-2.6.36rc4",), "exploits": (15023, 15024)},
+        "BCM": {"CVE": "2010-2959", "versions": ("2.6.0-2.6.36rc1",), "exploits": (14814,)},
+        "RDS protocol": {"CVE": "2010-3904", "versions": ("2.6.0-2.6.36rc8",), "exploits": (15285,)},
+        "put_user() - full-nelson": {"CVE": "2010-4258", "versions": ("2.6.0-2.6.37",), "exploits": (15704,)},
+        "sock_no_sendpage() - full-nelson": {"CVE": "2010-3849", "versions": ("2.6.0-2.6.37",), "exploits": (15704,)},
+        "ACPI custom_method": {"CVE": "2010-4347", "versions": ("2.6.0-2.6.37rc2",), "exploits": (15774,)},
+        "CAP_SYS_ADMIN": {"CVE": "2010-4347", "versions": ("2.6.34-2.6.37",), "exploits": (15916, 15944)},
+        "econet_sendmsg() - half-nelson": {"CVE": "2010-3848", "versions": ("2.6.0-2.6.36.2",), "exploits": (17787,)},
+        "ec_dev_ioctl() - half-nelson": {"CVE": "2010-3850", "versions": ("2.6.0-2.6.36.2",), "exploits": (17787, 15704)},
+        "ipc - half-nelson": {"CVE": "2010-4073", "versions": ("2.6.0-2.6.37rc1",), "exploits": (17787,)},
+    }
 
-    print "[+] Possible exploits:"
+    print("[+] Possible exploits:")
 
     for name, data in exploits.items():
-        versions = data["versions"] 
+        versions = data["versions"]
 
         for version_tree in versions:
             if "-" in version_tree:
@@ -141,21 +144,24 @@ def main():
 
                 for expl in exploits:
                     if isinstance(expl, int):
-                        exploits_str += "        %s/%d\n" % (exploitdb_url, expl)
+                        exploits_str += "        %s/%d\n" % (
+                            exploitdb_url, expl)
                     else:
-                        exploits_str += "        %s" % expl 
+                        exploits_str += "        %s" % expl
 
-                print """
+                print("""
 [*] Linux Kernel %s Local Root Exploit
     CVE: CVE-%s\n    Affects kernels: %s-%s
-    Exploits:\n%s""" % (name, cve, min_version, max_version, exploits_str)
+    Exploits:\n%s""") % (name, cve, min_version, max_version, exploits_str)
 
     if found_exploit:
         print
 
         if len(kernel_parts) > 1:
-            print "WARNING: %s appears to be a modified version of kernel %s." % (kernel_version_string, kernel_version)
-            print "These exploits can *possibly* get you to uid=0, but this script does *not* consider patched or backported kernel version\n"
+            print("WARNING: %s appears to be a modified version of kernel %s." % (
+                kernel_version_string, kernel_version))
+            print("These exploits can *possibly* get you to uid=0, but this script does *not* consider patched or backported kernel version\n")
+
 
 if __name__ == "__main__":
     main()
